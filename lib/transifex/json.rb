bd421c645f8259a2b3be22f7ddaa7123dac31668
Dante Cassanego <dante.cassanego@nubank.com.br>
2021-11-16 13:14:03 -0800

    adding debugging info

--- lib/transifex/json.rb
+++ lib/transifex/json.rb
@@ -3,18 +3,26 @@ module Transifex
     class << self
       if MultiJson.respond_to?(:dump)
         def dump(*args)
+          puts 'transifex::json'
+          puts *args
           MultiJson.dump(*args)
         end
 
         def load(*args)
+          puts 'transifex::json'
+          puts *args
           MultiJson.load(*args)
         end
       else
         def dump(*args)
+          puts 'transifex::json'
+          puts *args
           MultiJson.encode(*args)
         end
 
         def load(*args)
+          puts 'transifex::json'
+          puts *args
           MultiJson.decode(*args)
         end
       end
